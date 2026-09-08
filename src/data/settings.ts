b8f14e6f5d5d896c86d509f9dca9ff7536c62661
yugksuzuki <g-suzuki@hotmail.com>
2026-09-08 14:42:06 -0300

    comecar do zero

--- src/data/settings.ts
+++
@@ -1,6 +0,0 @@
-export const settings = {
-  site: 'https://www.inspmatch.com',
-  name: 'Inspmatch',
-  title: 'Inspmatch – Something New is Coming',
-  description: 'We are building something exciting that will change the way you experience online shopping. Stay tuned — Inspmatch is coming soon.',
-}
